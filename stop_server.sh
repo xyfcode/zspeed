de3ce3067d8f9e59199319be5955537ff559eb89
xyfcode <xyftprs@163.com>
2015-09-29 15:43:46 +0800

    this is the program's stop file

--- stop_server.sh
+++ stop_server.sh
@@ -1,5 +1,5 @@
-i1=`ps -ef|grep -E  "./node /home/project/moka/game/app.js" |grep -v grep| awk '{print $2}'`
-i2=`ps -ef|grep -E  "/home/project/moka/node /home/project/moka/game/app.js" |grep -v grep| awk '{print $2}'`
+i1=`ps -ef|grep -E  "./node /home/project/server/game/app.js" |grep -v grep| awk '{print $2}'`
+i2=`ps -ef|grep -E  "/home/project/server/node /home/project/server/game/app.js" |grep -v grep| awk '{print $2}'`
 
 kill -9 $i1
 kill -9 $i2
